54b37d87ea04d17909b3cc0bae8fb9248e2481d8
TDR <tdr.minecraft@gmail.com>
2023-02-19 13:16:27 +0100

    initial commit

---
+++ src/main/java/net/juligames/core/addons/scoreboard/ScoreboardPlugin.java
@@ -0,0 +1,16 @@
+package net.juligames.core.addons.scoreboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ScoreboardPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
